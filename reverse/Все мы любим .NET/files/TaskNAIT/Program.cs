661b3e6ad8ad1c1d10ed1ed2cdd20b9d8e674bdc
Marcus <166511746+MaRcUs1338@users.noreply.github.com>
2025-03-14 16:12:15 +0700

    Add files via upload

---
+++ reverse/Все мы любим .NET/files/TaskNAIT/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string flag = "NAITCTF{1ts_N41T_4ND_M3}";
+        Console.Write("Enter the flag: ");
+        string input = Console.ReadLine();
+
+        if (flag == input)
+        {
+            Console.WriteLine("Correct flag!");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("Wrong flag!");
+            Console.ReadKey();
+        }
+    }
+}
