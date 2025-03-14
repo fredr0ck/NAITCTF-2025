ce37da158f0f2b5252b898073529bca634fea8f0
Marcus <166511746+MaRcUs1338@users.noreply.github.com>
2025-03-14 16:57:09 +0700

    Add files via upload

---
+++ reverse/Великий и ужасный/files/ForObfC++/ForObfC++.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+#include <string> 
+
+int main() {
+    std::string password;
+
+    do {
+        std::cout << "Password:\n";
+        std::getline(std::cin, password);
+
+        if (password != "NAITCTF{d3bug_3v3ryth1ng}") {
+            std::cout << "Incorrect password. Repeat please..." << std::endl;
+        }
+    } while (password != "NAITCTF{d3bug_3v3ryth1ng}");
+
+    std::cout << "Password accepted\n";
+
+    return 0;
+}
